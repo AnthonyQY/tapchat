5a411fbbffa499eb0c3ba8ffcc2a5b50e1a31889
AnthonyQY <44106711+AnthonyQY@users.noreply.github.co>
2023-09-24 12:06:56 -0700

    Changed TextField layout

--- src/components/ChatWidget.tsx
+++ src/components/ChatWidget.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import {
   useHMSStore,
   selectHMSMessages,
@@ -37,45 +38,53 @@ export default function ChatWidget() {
   }, [allMessages]);
 
   return (
-    <Paper sx={{ height: isDesktop ? "53vh" : "25vh", maxWidth: "25rem" }}>
-      <Stack height={"inherit"} spacing={1}>
-        <Box paddingLeft={"1rem"} paddingTop={"0.5rem"}>
-          <Typography variant="h6">Chat</Typography>
-        </Box>
+    <Box padding={"5px"}>
+      <Paper sx={{ height: isDesktop ? "53vh" : "25vh", maxWidth: "25rem" }}>
+        <Stack height={"inherit"} spacing={1}>
+          <Box paddingLeft={"1rem"} paddingTop={"0.5rem"}>
+            <Typography variant="h6">Chat</Typography>
+          </Box>
 
-        <Divider />
-        <Stack
-          overflow={"scroll"}
-          paddingBottom={"1rem"}
-          paddingRight={"0.5rem"}
-        >
-          {allMessages.map((x) => (
-            <ChatMessage message={x} />
-          ))}
-        </Stack>
-        <Stack direction={"row"} marginTop={"auto !important"} width={"100%"}>
-          <TextField
-            label="Message"
-            placeholder="Your message"
-            fullWidth
-            onChange={(e: any) => setMessage(e.target.value)}
-            value={message}
-            onKeyDown={(e: any) => {
-              if (e.key == "Enter") {
-                handleSendMessage();
-              }
-            }}
-          />
-          <IconButton
-            aria-label="send message"
-            onClick={handleSendMessage}
-            color="primary"
-            sx={{ padding: "1rem" }}
+          <Divider />
+          <Stack
+            overflow={"scroll"}
+            paddingBottom={"1rem"}
+            paddingRight={"0.5rem"}
           >
-            <SendIcon />
-          </IconButton>
+            {allMessages.map((x) => (
+              <ChatMessage message={x} />
+            ))}
+          </Stack>
+          <Stack direction={"row"} marginTop={"auto !important"} width={"100%"}>
+            <TextField
+              label="Message"
+              placeholder="Your message"
+              fullWidth
+              onChange={(e: any) => setMessage(e.target.value)}
+              value={message}
+              variant="filled"
+              onKeyDown={(e: any) => {
+                if (e.key == "Enter") {
+                  handleSendMessage();
+                }
+              }}
+              InputProps={{
+                sx: { borderRadius: 0 },
+                endAdornment: (
+                  <IconButton
+                    aria-label="send message"
+                    onClick={handleSendMessage}
+                    color="primary"
+                    sx={{ padding: "1rem" }}
+                  >
+                    <SendIcon />
+                  </IconButton>
+                ),
+              }}
+            />
+          </Stack>
         </Stack>
-      </Stack>
-    </Paper>
+      </Paper>
+    </Box>
   );
 }
